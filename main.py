7b34179af03c7d39d94cec6364a221f02efdda3f
efratGoldshtoff <eg0548449793@gmail.com>
2026-01-06 00:19:52 +0200

    try

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+import gradio as gr
+
+def hello(nnn):
+    return f"hello {nnn}!"
+
 def main():
     print("Hello from myprojectllm!")
+    gr.Interface(
+        fn=hello,
+        inputs="text",
+        outputs="text"
+    ).launch()
+
 
 
 if __name__ == "__main__":
     main()
-    
+
